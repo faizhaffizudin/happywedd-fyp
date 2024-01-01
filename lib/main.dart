687c10a4d3a66ba54a2d1cc36ec2c63b98a06e47
hanisyamira <hanisyamira11@gmail.com>
2024-01-01 22:03:48 +0800

    buang debug

--- lib/main.dart
+++ lib/main.dart
@@ -190,6 +190,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: ToSanding(),
+      debugShowCheckedModeBanner: false,
     );
   }
 
